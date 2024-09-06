aaa8d43afd567b557d5b9e9787820036699c875b
pratikjadhav-01 <pratikjadhav4256@gmail.com>
2024-09-06 12:40:08 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+
+var accountPassword ="123478"
+const accountId = "10727226"
+let accountEmail = "pratik@googole.com"
+accountCity = "Mumbai"
+
+let accountState;
+
+console.log(accountId);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
